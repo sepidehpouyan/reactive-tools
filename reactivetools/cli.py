94f8432cbae7ade82c9816b0d9cf488c6d8110ba
Job Noorman <job.noorman@cs.kuleuven.be>
2015-12-15 14:59:04 +0100

    Add --verbose and --debug flags.
    These both control the amount of output generated. In addition, --debug
    will launch PDB on an uncaught exception.

--- reactivetools/cli.py
+++ reactivetools/cli.py
@@ -1,12 +1,40 @@
 import argparse
 import logging
 import asyncio
+import pdb
+import sys
 
 from . import config
 
 
+def _setup_logging(args):
+    if args.debug:
+        level = logging.DEBUG
+    elif args.verbose:
+        level = logging.INFO
+    else:
+        level = logging.WARNING
+
+    logging.basicConfig(format='%(message)s', level=level)
+
+
+def _setup_pdb(args):
+    if args.debug:
+        sys.excepthook = \
+                lambda type, value, traceback: pdb.post_mortem(traceback)
+
+
 def _parse_args(args):
     parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        '--verbose',
+        help='Verbose output',
+        action='store_true')
+    parser.add_argument(
+        '--debug',
+        help='Debug output and open PDB on uncaught exceptions',
+        action='store_true')
 
     subparsers = parser.add_subparsers(dest='command')
     # Workaround a Python bug. See http://bugs.python.org/issue9253#msg186387
@@ -32,6 +60,10 @@ def _handle_deploy(args):
 
 def main(raw_args=None):
     args = _parse_args(raw_args)
+
+    _setup_logging(args)
+    _setup_pdb(args)
+
     args.command_handler(args)
 
     # If we don't close the event loop explicitly, there is an unhandled
