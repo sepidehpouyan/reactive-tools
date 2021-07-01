932b6e1688fd7d1df64cd4d33427beed272ce693
Sepideh <sepideh.pouyanrad@cs.kuleuven.be>
2021-07-01 17:49:32 +0200

    attestaion header file is added for docker

---
+++ examples/example_trustzone/ta2/.ta.ld.d
@@ -0,0 +1,1 @@
+ta.lds: /optee/optee_os/out/arm/export-ta_arm32/src/ta.ld.S
---
+++ examples/example_trustzone/ta2/.ta2.o.d
@@ -0,0 +1,24 @@
+ta2.o: ta2.c /optee/optee_os/out/arm/export-ta_arm32/include/inttypes.h \
+ /optee/optee_os/out/arm/export-ta_arm32/include/stdint.h \
+ /optee/optee_os/out/arm/export-ta_arm32/include/limits.h \
+ /optee/optee_os/out/arm/export-ta_arm32/include/string.h \
+ /optee/toolchains/aarch32/lib/gcc/arm-none-linux-gnueabihf/10.2.1/include/stddef.h \
+ /optee/optee_os/out/arm/export-ta_arm32/include/sys/cdefs.h \
+ /optee/optee_os/out/arm/export-ta_arm32/include/stdio.h \
+ /optee/toolchains/aarch32/lib/gcc/arm-none-linux-gnueabihf/10.2.1/include/stdarg.h \
+ /optee/optee_os/out/arm/export-ta_arm32/include/stdlib.h \
+ /optee/optee_os/out/arm/export-ta_arm32/include/compiler.h \
+ /optee/optee_os/out/arm/export-ta_arm32/include/malloc.h \
+ /optee/optee_os/out/arm/export-ta_arm32/include/types_ext.h \
+ /optee/toolchains/aarch32/lib/gcc/arm-none-linux-gnueabihf/10.2.1/include/stdbool.h \
+ /optee/optee_os/out/arm/export-ta_arm32/include/unistd.h \
+ /optee/optee_os/out/arm/export-ta_arm32/include/tee_internal_api.h \
+ /optee/optee_os/out/arm/export-ta_arm32/include/tee_api.h \
+ /optee/optee_os/out/arm/export-ta_arm32/include/tee_api_defines.h \
+ /optee/optee_os/out/arm/export-ta_arm32/include/tee_api_types.h \
+ /optee/optee_os/out/arm/export-ta_arm32/include/trace.h \
+ /optee/optee_os/out/arm/export-ta_arm32/include/trace_levels.h \
+ /optee/optee_os/out/arm/export-ta_arm32/include/tee_ta_api.h \
+ include/ta2.h include/pta_attestation.h \
+ /optee/optee_os/out/arm/export-ta_arm32/include/util.h \
+ include/spongent.h
